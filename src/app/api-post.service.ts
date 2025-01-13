19b7b8ad17fe64281a3acffe2faeebae99b4b8fc
felip-araujo <felipearaujoexecutive@gmail.com>
2025-01-13 08:13:43 -0400

    servico-api-1

--- src/app/api-post.service.ts
+++ src/app/api-post.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiPostService {
+  private ApiUrl = 'https://jsonplaceholder.typicode.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getPosts(id: number): Observable<any> {
+    const url = `${this.ApiUrl}/posts`
+    return this.http.get(url);
+  }
 }
